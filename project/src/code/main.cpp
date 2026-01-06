cfc428109275413a1029b6627b71aae1934b2591
ghulamhabib364 <ghulamhabib.x@gmail.com>
2026-01-06 22:07:58 +0500

    main.cpp is set where program start it excution

---
+++ project/src/code/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+void clearScrean()
+{
+#ifndef _WIN32
+    system("c");
+#else
+    system("clear");
+#endif
+}
+using namespace std;
+int main()
+{
+    cout << "hellp World \n";
+    clearScrean();
+    return 0;
+}
