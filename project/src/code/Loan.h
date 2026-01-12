0d1aa233049e94776acdb10ae5eb4aedc0072d58
ghulamhabib364 <ghulamhabib.x@gmail.com>
2026-01-12 18:34:56 +0500

    loan class basic strutre added

---
+++ project/src/code/Loan.h
@@ -0,0 +1,32 @@
+#ifndef Loan_H
+#define Loan_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Loan
+{
+private:
+    // member variabl
+protected:
+    string type;
+    double amount;
+
+public:
+    // constructors
+    Loan();
+    virtual void setLoanAmount(double amount)
+    {
+    }
+    virtual double getLoanAmount()
+    {
+        return amount;
+    }
+};
+class homeLoan : public loan
+{
+
+} class CarLoan : public loan
+
+#endif // Loan_H
