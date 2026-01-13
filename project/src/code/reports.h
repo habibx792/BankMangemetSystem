da0745479cd7d76772850b1f7005e25b193b392d
ghulamhabib364 <ghulamhabib.x@gmail.com>
2026-01-13 18:40:53 +0500

    adding reports classes

---
+++ project/src/code/reports.h
@@ -0,0 +1,28 @@
+#ifndef Report_H
+#define Report_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Report {
+private:
+    // member variables
+public:
+    // constructors
+    Report();
+
+};
+class customerReort
+{
+
+}
+class atmReport
+{
+
+}
+class emplyeeRepory
+{
+    
+}
+#endif // Report_H
