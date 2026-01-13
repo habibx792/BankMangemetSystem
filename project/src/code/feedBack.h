787455194452120905fe025143af7c462adb78ee
ghulamhabib364 <ghulamhabib.x@gmail.com>
2026-01-14 00:43:40 +0500

    feedback class is added

---
+++ project/src/code/feedBack.h
@@ -0,0 +1,18 @@
+#ifndef FeedBack_H
+#define FeedBack_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class FeedBack {
+private:
+protected
+    // member variables
+public:
+    // constructors
+    FeedBack();
+
+};
+
+#endif // FeedBack_H
