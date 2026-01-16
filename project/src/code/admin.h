7a098b6cc61551992fbf85c7e8ab02b6cfeb2900
ghulamhabib364 <ghulamhabib.x@gmail.com>
2026-01-17 01:33:02 +0500

    onwer class and admin and man classes are added

---
+++ project/src/code/admin.h
@@ -0,0 +1,18 @@
+#ifndef Admin_H
+#define Admin_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Admin {
+private:
+protected:
+    // member variables
+public:
+    // constructors
+    Admin();
+
+};
+
+#endif // Admin_H
---
+++ project/src/code/owner.h
@@ -0,0 +1,18 @@
+#ifndef Owner_H
+#define Owner_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Owner {
+private:
+protected:
+    // member variables
+public:
+    // constructors
+    Owner();
+
+};
+
+#endif // Owner_H
