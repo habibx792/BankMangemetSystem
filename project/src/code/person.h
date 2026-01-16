399a4e3b3af07ebef47996046e5e1cefbc40b8e1
ghulamhabib364 <ghulamhabib.x@gmail.com>
2026-01-17 02:03:13 +0500

    person setter are added

---
+++ project/src/code/person.h
@@ -0,0 +1,83 @@
+#ifndef Person_H
+#define Person_H
+#include <string>
+#include <iostream>
+#include "dataHandle.h"
+using namespace std;
+DataHandle &dt = DataHandle::GetInstance();
+class Person
+{
+private:
+protected:
+    string personName;
+    string personFatherName;
+    int personAge;
+    string personCnic;
+    string personCountry;
+    string personCity;
+
+public:
+    // constructors
+    Person()
+    {
+        this->personAge = 0;
+        this->personName = "";
+        this->personCnic = "";
+        this->personFatherName = "";
+        this->personCountry = "";
+        this->personCity = "";
+    }
+    Person(string name, string cnic, int age, string fatherName, string country, string city)
+    {
+        this->personName = name;
+        this->personCnic = cnic;
+        if (dt.valiDateAge(age))
+        {
+            this->personAge = age;
+        }
+        else
+        {
+            dt.throughError();
+            cout << "Please Enter Correct Age \n";
+            personAge = 0;
+        }
+        this->personAge = age;
+        this->personFatherName = fatherName;
+        this->personCountry = country;
+        this->personCity = city;
+    }
+    void setPesonName(string name)
+    {
+        this->personName = name;
+    }
+    void setPersonCnic(string cnic)
+    {
+        this->personCnic = cnic;
+    }
+    void setPersonAge(int age)
+    {
+        if (dt.valiDateAge(age))
+        {
+            this->personAge = age;
+        }
+        else
+        {
+            dt.throughError();
+            cout << "Please Enter correct Age \n";
+        }
+    }
+    void setPersonFatherName(string fatherName)
+    {
+        this->personFatherName = fatherName;
+    }
+    void setPersonCountry(string country)
+    {
+        this->personCountry = country;
+    }
+    void setPersonCity(string city)
+    {
+        this->personCity = city;
+    }
+};
+
+#endif // Person_H
