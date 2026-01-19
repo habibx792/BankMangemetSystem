63a7848867600ac950caaceb0096dbb8f328ce34
Ghulam Habib <ghulamhabib.x@gmail.com>
2026-01-19 17:45:51 +0500

    i added salary class to project so we can create object of salary for seaching and paying salary

---
+++ project/src/code/Salary.h
@@ -0,0 +1,18 @@
+#ifndef Salary_H
+#define Salary_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Salary{
+private:
+protected:
+    // member variables
+public:
+    // constructors
+    Salary();
+
+};
+
+#endif // Salary_H
